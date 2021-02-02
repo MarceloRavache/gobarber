e16457fa40fe4237f3851e72d7018fd5eeac3aa3
MarceloRavache <jm.ravache@outlook.com>
2021-02-02 01:04:15 -0300

    update: tipo do id mudado para uuid

--- backend/src/database/migrations/1612148742422-CreateAppointments.ts
+++ backend/src/database/migrations/1612148742422-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1612148742422 implements MigrationInterfa
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
