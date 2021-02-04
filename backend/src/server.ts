573e7bb295a9346d0c7bbcc345d814be9566cab1
MarceloRavache <jm.ravache@outlook.com>
2021-02-04 00:39:47 -0300

    rotas appointments e users adicionados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import routes from './routes/appointments.routes';
+import routesAppointments from './routes/appointments.routes';
+import routesUsers from './routes/appointments.routes';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
+
+app.use("/appointments",routesAppointments);
+app.use("/users",routesUsers);
 
 app.listen(3300);
